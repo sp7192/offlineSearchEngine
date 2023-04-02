a0efec9c56001a1b0c3358cc7fd25121daa4d449
sina p <sp7192@gmail.com>
2023-04-02 20:46:16 +0430

    Adding first draft of configs

---
+++ configs/Configs.go
@@ -0,0 +1,29 @@
+package configs
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/viper"
+)
+
+type Configs struct {
+	EngineType string `mapstructure:"ENGINE_TYPE"`
+}
+
+func LoadConfigs(path string) (Configs, error) {
+	var ret Configs
+	viper.AddConfigPath(path)
+	viper.SetConfigName("configs")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err := viper.ReadInConfig()
+	if err != nil {
+		fmt.Println("fatal error config file: default \n", err)
+		os.Exit(1)
+	}
+	err = viper.Unmarshal(&ret)
+	return ret, err
+}
